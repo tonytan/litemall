e5290655e9a4802d4b121c40ff0a75af06856f22
tonytan <lion222@qq.com>
2018-12-14 11:15:57 +0800

    修改BASE_API
    Signed-off-by: tonytan <lion222@qq.com>

--- litemall-admin/config/dep.env.js
+++ litemall-admin/config/dep.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	NODE_ENV: '"production"',
-  ENV_CONFIG: '"dep"',
-  BASE_API: '"http://122.152.206.172:8080/admin"'
+  NODE_ENV: '"development"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"https://hw.guangfojiayuan.com/admin"'
 }
